d42b9ec7e8ff755aee5b4d770dbb62f9527b4116
vaylon-fernandes <vaylonfernandes@gmail.com>
2021-07-13 17:37:13 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="container center">
+        <h1> Hello World 👋</h1>
+        <h2> Welcome To My Page 😄 </h2>
+        <h3>To find out more about me and what I do click the buttons below</h3>
+        <Button buttonName="Home"></Button>
+        <Button buttonName="About"></Button>
+        <Button buttonName="Blog"></Button>
+        <Button buttonName="My Work"></Button>
+      </div>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+const Button = ({ buttonName }) => {
+  return <button className="button">{buttonName}</button>;
+};
+
+export default Button;
